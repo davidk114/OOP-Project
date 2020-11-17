2cedd6995572c0c938c2ec65f6aaa5ac6e17ba47
david <Dawid.Knurowski@students.ittralee.ie>
2020-11-17 14:45:26 +0000

    commited

--- src/RepairServiceSystem/RepairCase.java
+++ src/RepairServiceSystem/RepairCase.java
@@ -9,11 +9,68 @@ public class RepairCase {
     private String deviceBrand;
     private String status;
 
-public RepairCase(int CaseID){
-
-
-
+public RepairCase(int CaseID, Customer customer, String faultDesc,
+                  String serialNo, String deviceBrand, String status){
+    setCaseId(caseId);
+    setCustomer(customer);
+    setFaultDesc(faultDesc);
+    setSerialNo(serialNo);
+    setDeviceBrand(deviceBrand);
+    setStatus(status);
 }
 
+    public void setCaseId(int caseId) {
+        this.caseId = caseId;
+    }
 
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public void setDeviceBrand(String deviceBrand) {
+        this.deviceBrand = deviceBrand;
+    }
+
+    public void setFaultDesc(String faultDesc) {
+        this.faultDesc = faultDesc;
+    }
+
+    public void setSerialNo(String serialNo) {
+        this.serialNo = serialNo;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getCaseId() {
+        return caseId;
+    }
+
+    public String getDeviceBrand() {
+        return deviceBrand;
+    }
+
+    public String getFaultDesc() {
+        return faultDesc;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public String getSerialNo() {
+        return serialNo;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String toString(){
+    return "RC" + getCaseId() + "\n" + getCustomer() + "\n" +
+            "\nFault Description: " + getFaultDesc() + "\nDevice Type: " +
+            getDeviceBrand() + "\nSerial Number: " + getSerialNo() + "\n\nStatus: " +
+            getStatus();
+    }
 }
---
+++ src/RepairServiceSystem/TestRepairCase.java
@@ -0,0 +1,5 @@
+package RepairServiceSystem;
+
+public class TestRepairCase {
+
+}
